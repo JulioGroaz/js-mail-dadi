732eb784a73d74e0861c74bf66c49e2f7411c04f
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-13 15:29:35 +0200

    IF e ELSE creati

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -2,3 +2,8 @@ const mailSalvate = ["gino@mail.com","figaro@mail.com", "marta@mail.com", "franz
 
 const mailUtente = prompt("Inserisci la tua mail");
 
+if (mailSalvate.includes(mailUtente)){
+    console.log("Benvenut@!");
+} else{
+    console.log("Utente non trovato, prova con un'altra mail o riprova");
+}
