b180580b5408c5a1c70f84ae728a1e269c148782
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-13 15:49:41 +0200

    Stampato l'Array

--- Saltare a pie dispari/js/js/script.js
+++ Saltare a pie dispari/js/js/script.js
@@ -1,6 +1,9 @@
 const numeriPari = [];
 
 for (i=1 ; i<6; i++){
-    const numeroUtente = prompt("Inserisci un numero");
-    
+    const numeroUtente = parseInt(prompt("Inserisci un numero"),10);
+    if(numeroUtente %2 === 0 ){
+        numeriPari.push(numeroUtente);
+    }
 }
+console.log(numeriPari);
